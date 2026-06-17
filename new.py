38ea321456fee9531e8cd416d5a8948906ec311b
Muskan Agnihotri <muskan21agni@gmail.com>
2026-06-17 11:39:14 +0530

    thirdonl

--- new.py
+++ new.py
@@ -33,3 +33,40 @@ def calculate_category_spending(df):
 
     # TODO: Return the resulting Series
     return category_totals
+
+# 3. Plot Pie Chart for Category Spending Share
+def plot_category_pie(category_totals):
+    """
+    Create and save a pie chart of category spending distribution.
+    Shows which category (e.g., Dairy, Fruits) consumes the most budget.
+    """
+
+    # TODO: Set figure size to 7 inches by 7 inches
+    plt.figure(figsize=(7,7))
+    
+    # TODO: Create a pie chart
+    plt.pie(
+        # TODO: Use the values from category_totals
+        category_totals.values,
+
+        # TODO: Use the index (category names) as labels
+        labels=category_totals.index,
+
+        # TODO: Show percentage upto 1 decimal place
+        autopct='%1.1f%%',  
+
+        # TODO: Slightly separate (0.05) all the slices using explode
+        explode=[0.05]*len(category_totals)
+    )
+
+    # TODO: Set the plot title to "Grocery Spending by Category"
+    plt.title('Grocery Spending by Category')
+    
+    # Save the plot
+    output_file = "category_spending_pie_chart.png"
+    
+    # TODO: Save the figure to output_file with tight layout (bbox_inches='tight')
+    plt.savefig(output_file,bbox_inches='tight')
+    
+    print(f"Pie chart saved to {output_file}")
+
