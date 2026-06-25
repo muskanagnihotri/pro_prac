219143d05b163e0e5d53ff0f3ce938063c92b28e
Muskan Agnihotri <muskan21agni@gmail.com>
2026-06-25 15:28:58 +0530

    oneqes

---
+++ code.py
@@ -0,0 +1,11 @@
+# Teleport Home
+# Chef has traveled a long way, and now wants to get home.
+# Chef is 
+# D
+# D kilometers away from home, and he can walk at a speed of 
+# 1
+# 1 kilometer per hour.
+# Chef also has the ability to teleport. He can teleport for a distance of at most 
+# T kilometers, which happens instantly and doesn't require any time.
+# The teleport can be used at most once.
+# Find the minimum time, in hours, that Chef needs to reach home.
